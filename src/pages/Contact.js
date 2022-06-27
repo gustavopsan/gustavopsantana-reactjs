3cda633460dc3a591b8a4c28a07d512ce6dae9a5
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-06-27 09:49:40 -0300

    Implementação da lógica de envio das mensagens #2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -52,7 +52,7 @@ const Contact = () => {
             <p>Email: ${fields.email}</p>
             <p>Mensagem: ${fields.message}</p>`;
 
-            axios.post("http://api-gustavopsantana.herokuapp.com/send-message", {
+            axios.post("https://api-gustavopsantana.herokuapp.com/send-message", {
                 destination: "gustavopsantana4@gmail.com",
                 subject: "Novo contato através do site gustavopsantana.dev.br",
                 html: mailContent
