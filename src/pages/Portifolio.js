083ba44a6d0cf510e4f1a27cbd7e722f1f3ac981
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-06-26 21:08:35 -0300

    Criando modelo de página em manutenção

--- src/pages/Portifolio.js
+++ src/pages/Portifolio.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 import Container from "../components/Container";
+import Image from "../components/Image";
+import WhiteTitle from "../components/WhiteTitle";
+import { Text } from "../components/Text";
+
+import manutencaoBackgroung from '../assets/img/fundo-manutencao.jpg';
+import avisoImage from '../assets/img/aviso.png';
 
 const Portifolio = () => {
     return (
-        <Container>
-            <h1>Portifólio</h1>
+        <Container backgroundImage={manutencaoBackgroung}>
+            <Image src={avisoImage} className="maintence-logo" alt="Maintenance Logo"/>
+            <WhiteTitle>
+                Página em Manutenção!
+            </WhiteTitle>
+            <Text color="#F2F2F2">
+                Relaxa... Estou preparando algo legal por aqui e logo compartilho com vocês!
+            </Text>
         </Container>
     );
 }
