5298dacbab9596ce958407203d120c9a8c6851b9
Gustavo P. Santana <gustavopsantana4@gmail.com>
2022-06-26 23:03:04 -0300

    Preparando lógica para envio de formulário de contato

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 import Container from "../components/Container";
 import Aligner from "../components/Aligner";
@@ -9,6 +10,23 @@ import contactBackground from "../assets/img/contact-background.webp";
 import "../style/contact.scss";
 
 const Contact = () => {
+
+    const [fields, setFields] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+    
+    function handleInputChange(event) {
+        fields[event.target.id] = event.target.value;
+        setFields(fields);
+    }
+    
+    function handleMailSubmit(event) {
+        event.preventDefault();
+        console.log(fields);
+    }
+
     return (
         <Container backgroundImage={contactBackground}>
             <Aligner horizontal>
@@ -19,18 +37,18 @@ const Contact = () => {
                     <WhiteTitle>Entre em contato!</WhiteTitle>
                 </Aligner>
                 <Aligner vertical center>
-                    <form id="form-contact">
+                    <form id="form-contact" onSubmit={handleMailSubmit}>
                         <div className="form-group">
                             <label htmlFor="name">Nome</label>
-                            <input type="text" className="form-control" id="name" placeholder="Nome" />
+                            <input type="text" className="form-control" id="name" placeholder="Nome" onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Email" />
+                            <input type="email" className="form-control" id="email" placeholder="Email" onChange={handleInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="message">Mensagem</label>
-                            <textarea className="form-control" id="message" rows="3">Olá! Estou interessado(a) nos serviços informados pelo seu site! Por favor, entre em contato comigo!</textarea>
+                            <textarea className="form-control" id="message" rows="3" onChange={handleInputChange} defaultValue="Olá! Estou interessado(a) nos serviços informados pelo seu site! Por favor, entre em contato comigo!"></textarea>
                         </div>
                         <button type="submit" className="btn btn-primary">Deixar Mensagem</button>
                     </form>
